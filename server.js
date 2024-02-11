c90a41354895f50fbc55a5437fb7b134145484b4
Anglebert Ish <82755721+Anglebert-Dev@users.noreply.github.com>
2024-02-11 17:36:37 +0200

    send message

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Use the cors middleware
+app.use(cors());
+
+// Parse JSON requests
+app.use(express.json());
+
+app.post("/send-email", async (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  // Validate form data
+  if (!name || !email || !subject || !message) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  // Validate email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ error: "Invalid email format" });
+  }
+
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // Use SSL/TLS
+      auth: {
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: process.env.GMAIL_EMAIL,
+      subject: subject,
+      html: `
+      <h2>New Message from Contact Form</h2>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Subject:</strong> ${subject}</p>
+      <p><strong>Message:</strong></p>
+      <p>${message}</p>
+    `
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.response);
+    return res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return res.status(500).json({ error: "Error sending email" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
